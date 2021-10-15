21052e3511bc546511a9acfdfb6c418bf6175636
RCModesto <rctem@hotmail.com>
2021-10-14 23:28:20 -0300

    progress bar

--- static/script.js
+++ static/script.js
@@ -1,4 +1,11 @@
-/*document.addEventListener('contextmenu', event => event.preventDefault());
+//document.addEventListener('contextmenu', event => event.preventDefault());
+
+//Bloquear imagens
+$('img').bind('contextmenu', function(e) {
+    return false;
+});
+
+
 
 /* Abre e fecha menu lateral em modo mobile */
 
@@ -6,10 +13,10 @@ const menuMobile = document.querySelector(".menu-mobile");
 const body = document.querySelector("body");
 
 menuMobile.addEventListener("click", () => {
-  menuMobile.classList.contains("bi-list")
-    ? menuMobile.classList.replace("bi-list", "bi-x")
-    : menuMobile.classList.replace("bi-x", "bi-list");
-  body.classList.toggle("menu-nav-active");
+    menuMobile.classList.contains("bi-list") ?
+        menuMobile.classList.replace("bi-list", "bi-x") :
+        menuMobile.classList.replace("bi-x", "bi-list");
+    body.classList.toggle("menu-nav-active");
 });
 
 /* Fecha o menu quando clicar em algum item e muda o icone para list */
@@ -17,12 +24,12 @@ menuMobile.addEventListener("click", () => {
 const navItem = document.querySelectorAll(".nav-item");
 
 navItem.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (body.classList.contains("menu-nav-active")) {
-      body.classList.remove("menu-nav-active");
-      menuMobile.classList.replace("bi-x", "bi-list");
-    }
-  });
+    item.addEventListener("click", () => {
+        if (body.classList.contains("menu-nav-active")) {
+            body.classList.remove("menu-nav-active");
+            menuMobile.classList.replace("bi-x", "bi-list");
+        }
+    });
 });
 
 // Animar todos os itens na tela que tiverem meu atributo data-anime
@@ -30,21 +37,21 @@ navItem.forEach((item) => {
 const item = document.querySelectorAll("[data-anime]");
 
 const animeScroll = () => {
-  const windowTop = window.pageYOffset + window.innerHeight * 0.85;
+    const windowTop = window.pageYOffset + window.innerHeight * 0.85;
 
-  item.forEach((element) => {
-    if (windowTop > element.offsetTop) {
-      element.classList.add("animate");
-    } else {
-      element.classList.remove("animate");
-    }
-  });
+    item.forEach((element) => {
+        if (windowTop > element.offsetTop) {
+            element.classList.add("animate");
+        } else {
+            element.classList.remove("animate");
+        }
+    });
 };
 
 animeScroll();
 
 window.addEventListener("scroll", () => {
-  animeScroll();
+    animeScroll();
 })
 
 // Ativar carregamento no botão de enviar formulário para
@@ -53,12 +60,38 @@ const btnEnviar = document.querySelector('#btn-enviar')
 const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
 
 btnEnviar.addEventListener("click", () => {
-  btnEnviarLoader.style.display = "block";
-  btnEnviar.style.display = "none"
+    btnEnviarLoader.style.display = "block";
+    btnEnviar.style.display = "none"
 })
 
 // Tira a mensagem de sucesso depois de 5 segundos
 
 setTimeout(() => {
-  document.querySelector('#alerta').style.display = 'none';
+    document.querySelector('#alerta').style.display = 'none';
 }, 5000);
+
+//Barra de Progresso 
+$(".circle_percent").each(function() {
+    var $this = $(this),
+        $dataV = $this.data("percent"),
+        $dataDeg = $dataV * 3.6,
+        $round = $this.find(".round_per");
+    $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+    $this.append('<div class="circle_inbox"><span class="percent_text"></span></div>');
+    $this.prop('Counter', 0).animate({ Counter: $dataV }, {
+        duration: 2000,
+        easing: 'swing',
+        step: function(now) {
+            $this.find(".percent_text").text(Math.ceil(now) + "%");
+        }
+    });
+    if ($dataV >= 51) {
+        $round.css("transform", "rotate(" + 360 + "deg)");
+        setTimeout(function() {
+            $this.addClass("percent_more");
+        }, 1000);
+        setTimeout(function() {
+            $round.css("transform", "rotate(" + parseInt($dataDeg + 180) + "deg)");
+        }, 1000);
+    }
+});
